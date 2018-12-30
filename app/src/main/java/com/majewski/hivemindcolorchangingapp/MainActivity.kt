0bf67d4c8e544dce2a65127157e2083c8cdb0f8d
Bartosz Majewski <bartmaj@windowslive.com>
2018-12-30 21:09:04 +0100

    Layouts created.

--- app/src/main/java/com/majewski/hivemindcolorchangingapp/MainActivity.kt
+++ app/src/main/java/com/majewski/hivemindcolorchangingapp/MainActivity.kt
@@ -2,11 +2,36 @@ package com.majewski.hivemindcolorchangingapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_control.setOnClickListener {
+            setColorButtonsVisibility(View.VISIBLE)
+            setColorScreenVisibility(View.GONE)
+        }
+
+        btn_screen.setOnClickListener {
+            setColorScreenVisibility(View.VISIBLE)
+            setColorButtonsVisibility(View.GONE)
+        }
+    }
+
+    private fun setColorButtonsVisibility(visibility: Int) {
+        ib_blue.visibility = visibility
+        ib_orange.visibility = visibility
+        ib_purple.visibility = visibility
+        ib_red.visibility = visibility
+    }
+
+    private fun setColorScreenVisibility(visibility: Int) {
+        iv_color_display.visibility = visibility
     }
 }
